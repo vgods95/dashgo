3aef5d3a08e3e9b6b78869f168ab3acc4db3d2c0
Vinicius Godoy Ferreira <viniciusgodoyf@gmail.com>
2022-05-27 17:53:45 -0300

    configure chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../src/styles/theme';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
